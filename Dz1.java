18a1d204541a979ac4d515191c6276ff0b29b4bb
AnastasiiaNegro <50552968+AnastasiiaNegro@users.noreply.github.com>
2020-11-02 15:51:20 +0300

    Update Dz1.java

--- Dz1.java
+++ Dz1.java
@@ -2,19 +2,19 @@ package com.anastasiia;
 
 import java.util.Scanner;
 
-public class Dz1 {
+public class HomeWork1 {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner (System.in);
-        System.out.println("Enter count potatos:");
+        System.out.println("Enter count potatoes:");
         int countPotatos = scanner.nextInt();
         System.out.println("Enter count package:");
         int countPackage = scanner.nextInt();
         int countPotatosinPackage;
         countPotatosinPackage = countPotatos/countPackage;
-        System.out.println("Count potatos in package:" + countPotatosinPackage);
-        System.out.println("Remainder potatos:" + (countPotatos - (countPackage * countPotatosinPackage)));
+        System.out.println("Count potatoes in package:" + countPotatosinPackage);
+        System.out.println("Remainder potatoes:" + (countPotatos - (countPackage * countPotatosinPackage)));
 
     }
 }
