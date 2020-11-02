db42768f85c98a77fc1850259c28fcb278c6a3bc
Anastasiia Negro <AnastasiiaNegro@gmail.com>
2020-11-02 16:27:36 +0300

    new

---
+++ HW 3 with methods/src/com/anastasiia/Cat.java
@@ -0,0 +1,21 @@
+package com.anastasiia;
+
+public class Cat extends Animal {
+    public Cat(int age, int weight, String color) {
+        this.setAge(age);
+        this.setWeight(weight);
+        this.setColor(color);
+    }
+
+    @Override
+    public String toString() {
+        return "My Cat " + super.toString();
+
+    }
+
+    @Override
+    public String go() {
+        return "Jump";
+    }
+}
+
---
+++ HW3/Cat.java
@@ -0,0 +1,14 @@
+package com.anastasiia;
+
+public class Cat extends Animal {
+    public Cat(int age, int weight, String color) {
+        this.setAge(age);
+        this.setWeight(weight);
+        this.setColor(color);
+    }
+
+    @Override
+    public String toString() {
+        return "My cat " + super.toString();
+    }
+}
